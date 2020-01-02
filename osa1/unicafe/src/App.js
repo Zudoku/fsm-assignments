33f4673e80e60e7bcec3c5d6f289b06c6f19a2d4
Arttu Siren <arttu.siren@gmail.com>
2020-01-02 15:32:56 +0200

    implement the base app

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,9 +1,39 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom'
 
-const FeedBack = (props) => {
+const Button = ({ buttonText, onClick }) => {
   return (
-    <h2> Give feedback </h2>
+    <button onClick={onClick}>
+      {buttonText}
+    </button>
+  )
+}
+
+const FeedBack = ({ clickGood, clickNeutral, clickBad }) => {
+  return (
+    <div>
+      <h2> Give feedback </h2>
+      <Button buttonText="Good" onClick={clickGood} />
+      <Button buttonText="Neutral" onClick={clickNeutral} />
+      <Button buttonText="Bad" onClick={clickBad} />
+    </div>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  return (
+    <div>
+      <h2> Statistics </h2>
+      <div>
+        Good {good}
+      </div>
+      <div>
+        Neutral {neutral}
+      </div>
+      <div>
+        Bad {bad}
+      </div>
+    </div>
   )
 }
 
@@ -14,7 +44,16 @@ const App = () => {
 
   return (
     <div>
-      <FeedBack/>
+      <FeedBack 
+        clickGood={() => setGood(good + 1)}
+        clickNeutral={() => setNeutral(neutral + 1)}
+        clickBad={() => setBad(bad + 1)}
+      />
+      <Statistics 
+        good={good} 
+        neutral={neutral} 
+        bad={bad} 
+      />
     </div>
   )
 }
