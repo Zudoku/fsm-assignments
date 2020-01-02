2900b3ee66e34ba7ee9571b24947d9bcdc85e064
Arttu Siren <arttu.siren@gmail.com>
2020-01-02 17:10:16 +0200

    return 1.12

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -7,14 +7,28 @@ const App = ({ anecdotes }) => {
   }
 
   const [selected, setSelected] = useState(getRandomIndex(anecdotes.length))
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+  const currentVote = votes[selected]
+
+  const addVote = () => {
+    const nextVotes = [...votes]
+    nextVotes[selected] += 1
+    setVotes(nextVotes)
+  }
 
   return (
     <div>
       <div>
         {anecdotes[selected]}
       </div>
+      <div>
+        Has {currentVote}  votes.
+      </div>
       <br />
       
+      <button onClick={() => { addVote() } }>
+        Vote
+      </button>
       <button onClick={() => setSelected(getRandomIndex(anecdotes.length))}>
         Next anecdote
       </button>
