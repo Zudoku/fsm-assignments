143d18ca95c6f88407de7847710ffc8eb8f33588
Arttu Siren <arttu.siren@gmail.com>
2020-01-02 17:39:47 +0200

    return 1.14

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -1,14 +1,44 @@
 import React, { useState } from 'react'
 
+
+const Anecdote = ({ anecdote, title, votes }) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div>{anecdote}</div>
+      <div>has {votes} votes.</div>
+    </div>
+  )
+}
+
 const App = ({ anecdotes }) => {
 
   const getRandomIndex = (arrayLength) => {
     return Math.floor(Math.random() * Math.floor(arrayLength));
   }
 
+  const getMostVotesIndex = (votes) => {
+    let mostVotes = 0
+    for (let n in votes) {
+      const value = votes[n]
+      if (value > mostVotes) {
+        mostVotes = value
+      }
+    }
+
+    return votes.indexOf(mostVotes)
+  }
+
   const [selected, setSelected] = useState(getRandomIndex(anecdotes.length))
   const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
   const currentVote = votes[selected]
+  const mostVotesIndex = getMostVotesIndex(votes)
+
+  const mostVotedAnecdote = anecdotes[mostVotesIndex]
+  const mostVotedAnecdoteVoteAmount = votes[mostVotesIndex]
+
+  console.log(mostVotesIndex)
 
   const addVote = () => {
     const nextVotes = [...votes]
@@ -18,20 +48,18 @@ const App = ({ anecdotes }) => {
 
   return (
     <div>
-      <div>
-        {anecdotes[selected]}
-      </div>
-      <div>
-        Has {currentVote}  votes.
-      </div>
+      <Anecdote anecdote={anecdotes[selected]} title="Anecdote of the day" votes={currentVote} />
+
       <br />
-      
       <button onClick={() => { addVote() } }>
         Vote
       </button>
       <button onClick={() => setSelected(getRandomIndex(anecdotes.length))}>
         Next anecdote
       </button>
+      <br />
+
+      <Anecdote anecdote={mostVotedAnecdote} title="Anecdote with most votes" votes={mostVotedAnecdoteVoteAmount} />
     </div>
   )
 }
