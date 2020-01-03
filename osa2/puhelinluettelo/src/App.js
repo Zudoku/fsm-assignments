7e01d71bbcf54a7881a0ca78dd81066a6654fb51
Arttu Siren <arttu.siren@gmail.com>
2020-01-03 14:45:17 +0200

    support filtering with phone nnumbers or name

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -35,12 +35,25 @@ const AddNumberForm = ({ newName, newNumber, onSubmit, onNameChanged, onNumberCh
   )
 }
 
+const FilterNumbersForm = ({ onChange }) => {
+  return (
+    <div>
+      <div>Filter contacts with phone number or name:</div>
+      <input type="text" onChange={onChange} />
+    </div>
+  )
+}
+
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arttu Siren', phoneNumber: '+358-40-40404040' }
+    { name: 'Arttu Siren', phoneNumber: '+358-40-40404040' },
+    { name: 'Frederik karaoke', phoneNumber: '+358-55-555555' },
+    { name: 'Ilkka Lipsanen', phoneNumber: '+358-60-60606060' },
+    { name: 'Fernando Siren', phoneNumber: '+358-70-70707070' },
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filteredText, setFilteredText ] = useState('')
 
   const onSubmitAddNumber = (event) => {
     const foundPerson = persons.find( x => x.name.toLowerCase() === newName.toLowerCase())
@@ -68,9 +81,16 @@ const App = () => {
     event.preventDefault()
   }
 
+  const filteredPersons = persons.filter((x) => {
+    const comparedName = x.name.toLowerCase()
+    const comparedPhoneNumber = x.phoneNumber.toLowerCase()
+    const filter = filteredText.toLowerCase()
+    return comparedName.includes(filter) || comparedPhoneNumber.includes(filter)
+  })
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2>Add a contact</h2>
       <AddNumberForm
         newName={newName}
         newNumber={newNumber}
@@ -78,8 +98,11 @@ const App = () => {
         onNameChanged={(event) => { setNewName(event.target.value) }}
         onNumberChanged={(event) => { setNewNumber(event.target.value) }}
       />
-      <h2>Numbers</h2>
-      <PhoneNumberList persons={persons} />
+      <hr />
+      <h2>Filter contacts</h2>
+      <FilterNumbersForm onChange={(event) => setFilteredText(event.target.value)}/>
+      <h2>Contacts</h2>
+      <PhoneNumberList persons={filteredPersons} />
     </div>
   )
 
