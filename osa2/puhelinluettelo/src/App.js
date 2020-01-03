0e3377c218275c37ffeb38e1ab01b47cfd5353e0
Arttu Siren <arttu.siren@gmail.com>
2020-01-03 14:22:59 +0200

    support phone numbers

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
 
 const PhoneNumberList = ({ persons }) => {
-  const personsToRows = () => persons.map(person => <div key={person.name}>{person.name}</div>)
+const personsToRows = () => persons.map(person => <div key={person.name}>{person.name}  {person.phoneNumber}</div>)
   return (
     <div>{personsToRows()}</div>
   )
 }
 
-const AddNumberForm = ({ onSubmit, onNameChanged, onNumberChanged}) => {
+const AddNumberForm = ({ newName, newNumber, onSubmit, onNameChanged, onNumberChanged}) => {
   return (
     <form onSubmit={onSubmit} >
         <div>
@@ -18,10 +18,17 @@ const AddNumberForm = ({ onSubmit, onNameChanged, onNumberChanged}) => {
           />
         </div>
         <div>
+          number: 
+          <input 
+            onChange={onNumberChanged}
+            value={newNumber}
+          />
+        </div>
+        <div>
           <button
             type="submit"
           >
-              add
+            add
           </button>
         </div>
       </form>
@@ -30,23 +37,33 @@ const AddNumberForm = ({ onSubmit, onNameChanged, onNumberChanged}) => {
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arttu Siren', phoneNumber: '+358-40-40404040' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const onSubmitAddNumber = (event) => {
     const foundPerson = persons.find( x => x.name.toLowerCase() === newName.toLowerCase())
+    // Check if already found
     if (foundPerson !== undefined) {
       alert(`${newName} can already be found from the phonebook.`)
       event.preventDefault()
       return
     }
+    // Check if either field is empty
+    if (newName === "" || newNumber === "") {
+      alert('Please fill out the form completely.')
+      event.preventDefault()
+      return
+    }
 
     const newPresons = [ ...persons, {
-      name: newName
+      name: newName,
+      phoneNumber: newNumber
     }]
     setPersons(newPresons)
     setNewName('')
+    setNewNumber('')
 
     event.preventDefault()
   }
@@ -55,8 +72,11 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <AddNumberForm
+        newName={newName}
+        newNumber={newNumber}
         onSubmit={onSubmitAddNumber}
-        onNameChanged={(event) => { setNewName(event.target.value) }} 
+        onNameChanged={(event) => { setNewName(event.target.value) }}
+        onNumberChanged={(event) => { setNewNumber(event.target.value) }}
       />
       <h2>Numbers</h2>
       <PhoneNumberList persons={persons} />
